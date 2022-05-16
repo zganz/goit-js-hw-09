57c43542a0b0e742656fd449b283b3174760e0c0
zganz <humanmailer@gmail.com>
2022-05-16 18:31:58 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const body = document.querySelector("body");
+  console.log(body);
+  body.addEventListener('click', setBodyColor);
+
+  function setBodyColor(){
+    const color = getRandomHexColor();
+    body.style.backgroundColor = color;
+  }
