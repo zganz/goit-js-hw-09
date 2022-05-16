0c7b5cfc60ad0653b31494b3995054ac35fdc4d7
zganz <humanmailer@gmail.com>
2022-05-16 21:32:24 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,12 +1,31 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
+  const body = document.querySelector('body');
+//   const sartBtn = document.querySelector('[data-start]');
+//   const stopBtn = document.querySelector('[data-stop]');
+//   sartBtn.addEventListener('click', setBodyColor);
+//   stopBtn.addEventListener('click', clearBodyColor);
 
-  const body = document.querySelector("body");
-  console.log(body);
-  body.addEventListener('click', setBodyColor);
+body.addEventListener('click', findClick);
+
+function findClick(evt){
+    if(evt.target.hasAttribute("data-start")){
+        setInterval(setBodyColor, 1000);
+    }
+    
+}
+
+
 
   function setBodyColor(){
     const color = getRandomHexColor();
     body.style.backgroundColor = color;
+    
   }
+
+//   function clearBodyColor(){
+//     clearInterval(setBodyColor, 1000);
+//     body.style.backgroundColor = color;
+
+//   }
