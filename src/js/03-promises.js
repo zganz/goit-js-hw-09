3848280ad415040bffde1772b84598c6d8741d9b
zganz <humanmailer@gmail.com>
2022-05-22 17:18:32 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+
+form = document.querySelector('form'),
+firstDelay = document.querySelector('[name="delay"]'),
+stepDelay = document.querySelector('[name="step"]'),
+amountEl = document.querySelector('[name="amount"]'),
+
+
+form.addEventListener('submit', clickBtn);
+
+function clickBtn(event) {
+   event.preventDefault();
+  const delay = Number(firstDelay.value);
+  const step = Number(stepDelay.value);
+  const amount = Number(amountEl.value);
+  let delayStep = delay;
+  for (let i = 1; i <= amount; i++) {
+    
+    createPromise(i, delayStep)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delayStep += step;
+  };
+
+  
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.5;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay } );
+      }
+      else {
+        reject({ position, delay });
+      }
+    }, delay)
+  })
+  
+};
